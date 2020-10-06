dd55b644696aaeeaa0c1c6a6d8f17d3195e2b592
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-06 11:29:32 +0200

    initial commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,56 +1,126 @@
-import React from 'react'
-import { MTRow, MTColumn } from 'mt-ui'
-import ExcelDropzone from './excel-dropzone.jsx'
+import React from 'react';
+import { MTRow, MTColumn } from 'mt-ui';
+import ExcelDropzone from './excel-dropzone.jsx';
+import users from './users';
+import scores from './scores';
+// styles
 
 export default class Main extends React.Component {
-  handleSheetData (data) {
-    // replace this log with actual handling of the data
-    console.log(data)
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsonData: '',
+      loadingData: '',
+    };
+    this.handleSheetData = this.handleSheetData.bind(this);
+    this.handleSheetStartupData = this.handleSheetStartupData.bind(this);
   }
 
-  render () {
+  componentDidMount() {
+    this.handleSheetStartupData();
+  }
+
+  handleSheetData(data) {
+    // replace this log with actual handling of the data
+    // console.log(data)
+
+    // Sort data ascending or descending
+    data.sort((a, b) => a.score - b.score).reverse();
+
+    // Set data to jsonData state variable
+    this.setState({
+      jsonData: data,
+    });
+  }
+
+  handleSheetStartupData() {
+    let iniItems;
+    let scoreItems = { scores };
+
+    iniItems = users.map((user, key) => {
+      let scoreData;
+      let maxResultUser;
+
+      scoreData = scoreItems.scores.filter((scoreId) => scoreId.userId === user._id);
+      maxResultUser = scoreData.sort((a, b) => a.score - b.score).reverse()[0];
+
+      return <div key={key}>{user.name + ' - ' + maxResultUser.score}</div>;
+    });
+
+    // Set highest scores
+    this.setState({
+      loadingData: iniItems
+        .sort(
+          (a, b) =>
+            a.props.children.split('-')[1].trimStart() - b.props.children.split('-')[1].trimStart(),
+        )
+        .reverse(),
+    });
+  }
+
+  render() {
+    let items;
+    // check if jsonData is null if not return items with map funtion
+    if (this.state.jsonData) {
+      items = this.state.jsonData.map((data, key) => {
+        return <div key={key}>{data.name + ' - ' + data.score}</div>;
+      });
+    }
+
     return (
       <div className="container container--centered">
         <h1 className="m-t">Mediatool exercise</h1>
         <MTRow>
-          <MTColumn width={ 20 }>
-            <ExcelDropzone
-              onSheetDrop={ this.handleSheetData }
-              label="Drop your file here"
-            />
+          <MTColumn width={20}>
+            <ExcelDropzone onSheetDrop={this.handleSheetData} label="Drop your file here" />
           </MTColumn>
-          <MTColumn width={ 75 } offset={ 5 }>
+          <MTColumn width={75} offset={5}>
             <div>
               <h2>Initial site</h2>
               <p>
-                Drop the excel file scores.xlsx that you will find
-                in this repo in the area to the left and watch the log output in the console.
-                We hope this is enough to get you started with the import.
+                Drop the excel file scores.xlsx that you will find in this repo in the area to the
+                left and watch the log output in the console. We hope this is enough to get you
+                started with the import.
               </p>
             </div>
             <div>
               <h2>Explaining the grid</h2>
               <p>
-                In the Mediatool grid you can use MTRow and MTColumn
-                to structure your graphical components.
-                This is basically what you need to know:
+                In the Mediatool grid you can use MTRow and MTColumn to structure your graphical
+                components. This is basically what you need to know:
               </p>
               <ul>
                 <li>
-                  The index.jsx file uses these components so you
-                  can see an example of how they work
+                  The index.jsx file uses these components so you can see an example of how they
+                  work
                 </li>
                 <li>MTRow will always create a line break</li>
                 <li>
-                  MTColumns will stretch to the width of the entire row,
-                  unless you use the properties width and offset
+                  MTColumns will stretch to the width of the entire row, unless you use the
+                  properties width and offset
                 </li>
                 <li>Width and offset is set in percent</li>
               </ul>
             </div>
           </MTColumn>
         </MTRow>
+
+        <MTRow>
+          <MTColumn className="startup-results" width={20}>
+            <div>
+              <h2>Startup results</h2>
+              {this.state.loadingData}
+            </div>
+          </MTColumn>
+
+          <MTColumn className="dropped-file-results" width={20}>
+            <div>
+              <h2>Dropped file results</h2>
+              {items}
+            </div>
+          </MTColumn>
+        </MTRow>
       </div>
-    )
+    );
   }
 }

bef154ad31a7cdba1f7f8c58a268cd65a6536b43
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-08 16:28:13 +0200

    User table

--- src/components/Table/UserTable.jsx
+++ src/components/Table/UserTable.jsx
@@ -1,70 +1,33 @@
 import React from 'react';
-import EditDataForm from '../Form/EditDataForm';
 
-export default class UserTable extends React.Component {
-  constructor(props) {
-    super(props);
-    //state
-    this.state = {
-      jsonData: '',
-      loadingData: '',
-      userAdded: 'false',
-      //editing: 'false',
-      setEditing: 'false',
-    };
-  }
+const UserTable = (...props) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Score</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{props[0].props.data.props.children.split('-')[0].trimStart()}</td>
+          <td>{props[0].props.data.props.children.split('-')[1].trimStart()}</td>
+          <td>
+            <button
+              className="button-primary"
+              onClick={() =>
+                props[0].props.met(props[0].props.data.props.children.split('-')[0].trim())
+              }
+            >
+              Show All User Scores
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
-  /* editUser = (_id, user) => {
-    setEditing(true);
-    setCurrentUser(user);
-  };
-
-  updateUser = (newUser) => {
-    setUsers(users.map((user) => (user.id === currentUser.id ? newUser : user)));
-    setCurrentUser(initialUser);
-    setEditing(false);
-  };
-*/
-  handleClick = () => {};
-
-  // Edit function
-  onDataEdit = (name) => {
-    const new_data = this.state.loadingData;
-
-    let userId = new_data.length + 1;
-    new_data.push(<div key={userId}>{name + ' - ' + 0}</div>);
-    new_data
-      .sort(
-        (a, b) =>
-          a.props.children.split('-')[1].trimStart() - b.props.children.split('-')[1].trimStart(),
-      )
-      .reverse();
-
-    this.setState({
-      loadingData: new_data,
-      setEditing: 'true',
-    });
-  };
-  render() {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Score</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{this.props.children.split('-')[0].trimStart()}</td>
-            <td>{this.props.children.split('-')[1].trimStart()}</td>
-            <td>
-              <EditDataForm onEdit={this.onDataEdit} />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
-}
+export default UserTable;

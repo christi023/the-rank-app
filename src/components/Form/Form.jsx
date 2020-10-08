d6455768cfd0f349ee6ad5c37aee8aa6314f2ba0
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-08 22:39:18 +0200

    Form to input / add data

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,10 +5,12 @@ export default class Form extends React.Component {
     super(props);
     this.state = {
       name: '',
+      error: '',
     };
     // binding
     this.addData = this.addData.bind(this);
     this.onDataChange = this.onDataChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onDataChange = (event) => {
@@ -24,9 +26,32 @@ export default class Form extends React.Component {
     });
   };
 
+  validate = (e) => {
+    let error = '';
+    if (!this.state.name) {
+      error = 'Data cannot be blank';
+    }
+    if (error) {
+      this.setState({ error });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const isValid = this.validate(event);
+    if (isValid) {
+      console.log(this.state);
+      // clear form
+      this.setState(this.state);
+    }
+  };
+
   render() {
     return (
-      <div className="form">
+      <form className="form" onSubmit={this.handleSubmit}>
         <input
           className="form-input"
           onChange={this.onDataChange}
@@ -34,13 +59,14 @@ export default class Form extends React.Component {
           type="text"
           placeholder="Add Data = name - score"
         />
+        <div style={{ fontSize: 12, color: 'red' }}>{this.state.error}</div>
         <button className="btn-secondary" onClick={this.addData}>
           Add Data
         </button>
 
         <br />
         <br />
-      </div>
+      </form>
     );
   }
 }

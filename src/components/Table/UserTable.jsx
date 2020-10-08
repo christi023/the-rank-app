9662b8468f0002cd64ddc47192dd78e6c849a448
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-08 09:18:40 +0200

    added user table with functionality

--- src/components/Table/UserTable.jsx
+++ src/components/Table/UserTable.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+//import EditDataForm from '../Form/EditDataForm';
 const UserTable = (props) => {
+  const initialUser = { _id: null, name: '', score: '' };
+  const [users, setUsers] = useState(initialUser);
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState(initialUser);
+
+  const handleClick = (e) => {
+    alert('edit_row');
+  };
+
+  const editUser = (_id, user) => {
+    setEditing(true);
+    setCurrentUser(user);
+  };
+
+  const updateUser = (newUser) => {
+    setUsers(users.map((user) => (user.id === currentUser.id ? newUser : user)));
+    setCurrentUser(initialUser);
+    setEditing(false);
+  };
+
   return (
     <table>
       <thead>
@@ -16,7 +36,17 @@ const UserTable = (props) => {
           <td>{props.children.split('-')[1].trimStart()}</td>
           <td>
             <button onClick={() => props.deleteUser()}>Delete</button>
-            <button onClick={() => props.editUser()}>Edit</button>
+            <button
+              id="edit_button2"
+              value="Edit"
+              className="edit"
+              onClick={() => handleClick(editUser())}
+            >
+              Edit
+            </button>
+            <button id="save_button2" value="Save" className="save" onClick={() => handleClick()}>
+              Save
+            </button>
           </td>
         </tr>
       </tbody>

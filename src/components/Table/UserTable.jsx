8b4cd0c57913d458b66508d3e6ef01634fe7e036
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-08 11:38:28 +0200

    added user table with functionality

--- src/components/Table/UserTable.jsx
+++ src/components/Table/UserTable.jsx
@@ -1,56 +1,70 @@
-import React, { useState } from 'react';
-//import EditDataForm from '../Form/EditDataForm';
-const UserTable = (props) => {
-  const initialUser = { _id: null, name: '', score: '' };
-  const [users, setUsers] = useState(initialUser);
-  const [editing, setEditing] = useState(false);
-  const [currentUser, setCurrentUser] = useState(initialUser);
+import React from 'react';
+import EditDataForm from '../Form/EditDataForm';
 
-  const handleClick = (e) => {
-    alert('edit_row');
-  };
+export default class UserTable extends React.Component {
+  constructor(props) {
+    super(props);
+    //state
+    this.state = {
+      jsonData: '',
+      loadingData: '',
+      userAdded: 'false',
+      //editing: 'false',
+      setEditing: 'false',
+    };
+  }
 
-  const editUser = (_id, user) => {
+  /* editUser = (_id, user) => {
     setEditing(true);
     setCurrentUser(user);
   };
 
-  const updateUser = (newUser) => {
+  updateUser = (newUser) => {
     setUsers(users.map((user) => (user.id === currentUser.id ? newUser : user)));
     setCurrentUser(initialUser);
     setEditing(false);
   };
+*/
+  handleClick = () => {};
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Score</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{props.children.split('-')[0].trimStart()}</td>
-          <td>{props.children.split('-')[1].trimStart()}</td>
-          <td>
-            <button onClick={() => props.deleteUser()}>Delete</button>
-            <button
-              id="edit_button2"
-              value="Edit"
-              className="edit"
-              onClick={() => handleClick(editUser())}
-            >
-              Edit
-            </button>
-            <button id="save_button2" value="Save" className="save" onClick={() => handleClick()}>
-              Save
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  );
-};
-export default UserTable;
+  // Edit function
+  onDataEdit = (name) => {
+    const new_data = this.state.loadingData;
+
+    let userId = new_data.length + 1;
+    new_data.push(<div key={userId}>{name + ' - ' + 0}</div>);
+    new_data
+      .sort(
+        (a, b) =>
+          a.props.children.split('-')[1].trimStart() - b.props.children.split('-')[1].trimStart(),
+      )
+      .reverse();
+
+    this.setState({
+      loadingData: new_data,
+      setEditing: 'true',
+    });
+  };
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Score</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{this.props.children.split('-')[0].trimStart()}</td>
+            <td>{this.props.children.split('-')[1].trimStart()}</td>
+            <td>
+              <EditDataForm onEdit={this.onDataEdit} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
